6afb2f8e90592d8d2c3e39083f1eac903dcb0d76
Pola <polaskibinska@gmail.com>
2020-09-18 15:21:38 +0200

    Counter Panel added

---
+++ js/counter.js
@@ -0,0 +1,39 @@
+const peaks = [
+  "Rysy",
+  "Babia Góra",
+  "Śnieżka",
+  "Śnieżnik",
+  "Tarnica",
+  "Turbacz",
+  "Radziejowa",
+  "Skrzyczne",
+  "Mogielica",
+  "Wysoka Kopa",
+  "Rudawiec",
+  "Orlica",
+  "Wysoka",
+  "Wielka Sowa",
+  "Lackowa",
+  "Kowadło",
+  "Jagodna",
+  "Skalnik",
+  "Waligóra",
+  "Czupel",
+  "Szczeliniec Wielki",
+  "Lubomir",
+  "Biskupia Kopa",
+  "Chełmiec",
+  "Kłodzka Góra",
+  "Skopiec",
+  "Ślęża",
+  "Łysica",
+];
+
+const counter = document.querySelector("#counter");
+let peaksConquered = JSON.parse(localStorage.getItem("items"));
+
+peaksConquered = peaksConquered.filter(peak => {
+    return peak.done == true && peaks.includes(peak.text);
+});
+
+counter.innerHTML = `${peaksConquered.length} z 28`;
