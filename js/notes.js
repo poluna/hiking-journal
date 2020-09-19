34b93f893cc74400c1ddebbbfadb36cc7ed32a1b
Pola <polaskibinska@gmail.com>
2020-09-19 19:17:19 +0200

    Notes about places to see added

--- js/notes.js
+++ js/notes.js
@@ -2,18 +2,30 @@ const badges = `— Odznaka Zdobywcy Korony Gór Polski
 — Popularna Górska Odznaka Turystyczna PTTK
 — Mała Brązowa Górska Odznaka Turystyczna PTTK`;
 
+const places = `Smreczyński Staw 
+– polodowcowe jezioro morenowe w polskich Tatrach Zachodnich
+
+Wodospad Szklarki
+– drugi co do wysokości, po Wodospadzie Kamieńczyka (27 m wysokości) wodospad w polskich Karkonoszach, położony na wysokości 525 m n.p.m.
+`;
+
 const badgesNote = document.querySelector("#badges");
+const placesNote = document.querySelector("#places");
 
 function setNotes(note = "", place) {
   place.innerHTML = note;
 }
 
-function updateNote(title) {
-  const note = badgesNote.value;
+function updateNote(place, title) {
+  const note = place.value;
   localStorage.setItem(title, JSON.stringify(note));
 }
 
-note = JSON.parse(localStorage.getItem("notes")) || badges;
-setNotes(note, badgesNote);
+note1 = JSON.parse(localStorage.getItem("badges")) || badges;
+setNotes(note1, badgesNote);
 
-badgesNote.addEventListener("input", updateNote.bind(this, "badges"));
+note2 = JSON.parse(localStorage.getItem("places")) || places;
+setNotes(note2, placesNote);
+
+badgesNote.addEventListener("input", (e) => updateNote(e.target, "badges"));
+placesNote.addEventListener("input", (e) => updateNote(e.target, "places"));
